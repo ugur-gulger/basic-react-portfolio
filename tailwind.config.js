5a67425a74392922f8189fbf36f0abafb89c27aa
Uğur Gülger <ugurgulger@Ugurs-MacBook-Air.local>
2022-10-15 23:57:22 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
+    },
+  },
+  plugins: [],
+};
